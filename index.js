ea706c251383b94184c8aec7648ea4fee5d78e6d
Diego Briceño <diegobrice@hotmail.com>
2020-07-30 15:58:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// const profile = document.querySelector(".home__profile");
+
+// window.addEventListener("scroll", function () {
+//   let value = window.scrollY / 100;
+//   profile.style.transform = `scale(${value})`;
+// });
