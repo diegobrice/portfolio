effc906a09baba8f5f981129fe77db5b32410ca8
Diego Briceño <diegobrice@hotmail.com>
2020-07-31 01:20:06 -0500

    fixed services

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const navbar = document.querySelector(".navbar");
+// const elements = document.querySelectorAll(".services__item");
 
 window.addEventListener("scroll", function () {
   window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
   window.scrollY < 100 && (navbar.style.background = `transparent`);
 });
+
+// window.addEventListener("mousemove", parallax);
+
+// function parallax(e) {
+//   elements.forEach((item) => {
+//     const speed = item.getAttribute("data-speed");
+//     console.log(speed);
+//     const x = (window.innerWidth - e.pageX * speed) / 100;
+//     const y = (window.innerHeight - e.pageY * speed) / 100;
+//     item.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//   });
+// }
