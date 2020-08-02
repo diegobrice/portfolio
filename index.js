216fb784a66ee9138d4e721c1c63847fefabf0fd
Diego Briceño <diegobrice@hotmail.com>
2020-08-01 22:31:14 -0500

    navbar responsive

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const navbar = document.querySelector(".navbar");
 const homebg = document.querySelector(".home__background");
-// const elements = document.querySelectorAll(".services__item");
-
-window.addEventListener("scroll", function () {
-  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
-  window.scrollY < 100 && (navbar.style.background = `transparent`);
-  // homebg.style.backgroundPosition = `-${window.scrollY * 2}px`;
-});
-
-// window.addEventListener("mousemove", parallax);
-
-// function parallax(e) {
-//   elements.forEach((item) => {
-//     const speed = item.getAttribute("data-speed");
-//     console.log(speed);
-//     const x = (window.innerWidth - e.pageX * speed) / 100;
-//     const y = (window.innerHeight - e.pageY * speed) / 100;
-//     item.style.transform = `translateX(${x}px) translateY(${y}px)`;
-//   });
-// }
-
+const menu = document.querySelector(".navbar__menu");
+const links = document.querySelector(".navbar__links");
 const sections = document.querySelectorAll("section");
 const config = {
   rootMargin: "0px",
   threshold: [0.6, 0.9],
 };
+
+function handleLlinks() {
+  if (window.innerWidth <= 991) {
+    links.classList.toggle("visible");
+  }
+}
+
+menu.addEventListener("click", handleLlinks);
+links.addEventListener("click", handleLlinks);
+
+window.addEventListener("scroll", function () {
+  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+  window.scrollY < 100 && (navbar.style.background = `transparent`);
+});
 
 let observer = new IntersectionObserver(function (entries, self) {
   entries.forEach((entry) => {
@@ -52,3 +48,15 @@ function intersectionHandler(entry) {
     shouldBeActive.classList.add("active");
   }
 }
+
+// window.addEventListener("mousemove", parallax);
+
+// function parallax(e) {
+//   elements.forEach((item) => {
+//     const speed = item.getAttribute("data-speed");
+//     console.log(speed);
+//     const x = (window.innerWidth - e.pageX * speed) / 100;
+//     const y = (window.innerHeight - e.pageY * speed) / 100;
+//     item.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//   });
+// }
