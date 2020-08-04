b76e5d0da38ee31960f36ea73aed8462206e9509
Diego Briceño <diegobrice@hotmail.com>
2020-08-04 12:18:49 -0500

    added scroll reveal animation

--- index.js
+++ index.js
@@ -61,15 +61,19 @@ function intersectionHandler(entry) {
 //   });
 // }
 
-ScrollReveal().reveal(".navbar", { delay: 350 });
+ScrollReveal().reveal(".navbar", { delay: 250 });
 ScrollReveal().reveal(".home__profile", { delay: 350 });
-ScrollReveal().reveal(".home__description", { delay: 350 });
-ScrollReveal().reveal(".section__title", { delay: 350 });
+ScrollReveal().reveal(".home__title--primary", { delay: 350 });
+ScrollReveal().reveal(".home__title", { delay: 450 });
+ScrollReveal().reveal(".home__title--secondary", { delay: 550 });
+ScrollReveal().reveal(".section__title", { delay: 250 });
 ScrollReveal().reveal(".section__subtitle", { delay: 350 });
 ScrollReveal().reveal(".about__description", { delay: 350 });
-ScrollReveal().reveal(".skill__title", { delay: 350 });
-ScrollReveal().reveal(".skill__item", { delay: 350 });
-ScrollReveal().reveal(".services__item", { delay: 350 });
-ScrollReveal().reveal(".portfolio__item", { delay: 350 });
-ScrollReveal().reveal(".contact__item", { delay: 350 });
-ScrollReveal().reveal(".footer", { delay: 350 });
+ScrollReveal().reveal(".about__summary", { delay: 450 });
+ScrollReveal().reveal(".button--cta", { delay: 550 });
+ScrollReveal().reveal(".skill__title", { delay: 450 });
+ScrollReveal().reveal(".skill__item", { delay: 450 });
+ScrollReveal().reveal(".services__item", { delay: 450 });
+ScrollReveal().reveal(".portfolio__item", { delay: 450 });
+ScrollReveal().reveal(".contact__item", { delay: 450 });
+ScrollReveal().reveal(".footer", { delay: 450 });
